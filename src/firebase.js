49a330649e309a34f9df099d727145e2c4237b7f
johanhobbs <johanhobbsnz@gmail.com>
2023-10-30 12:50:01 +1300

    more firebase stuff and login

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app Firebase Config
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjg1gquFkHsPsDb7fLbBw71c5yIaAcepQ",
+  authDomain: "fir-auth-b02bf.firebaseapp.com",
+  projectId: "fir-auth-b02bf",
+  storageBucket: "fir-auth-b02bf.appspot.com",
+  messagingSenderId: "990688751252",
+  appId: "1:990688751252:web:3baeb80bbe36a405ca4e81",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+//const analytics = firebaseApp.getAnalytics(app);
